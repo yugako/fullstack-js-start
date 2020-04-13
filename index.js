89f6731cb8f3475fda7bf91a92e1db51b8531090
Yurii Koltso <yura.k.upqode@gmail.com>
2020-04-13 22:57:19 +0300

    Client

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 // const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json({extended: true}));
 
