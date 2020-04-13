afe2c7057c520409406d5d9953c608f93088fa1e
Yurii Koltso <yura.k.upqode@gmail.com>
2020-04-13 22:45:32 +0300

    Entry file

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+// const config = require('config');
+// const mongoose = require('mongoose');
+// const path = require('path');
+// const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({extended: true}));
+
+/**
+ * Serve static files from the React app
+ */
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+/**
+ * Set api routes
+*/
+
+// app.use('/api/your-route', require('./routes/your-route.routes'));
+
+
+/**
+ * The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+*/
+
+/*
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    });
+*/
+
+
+/** Start server */
+
+// Start server using Mongo
+
+/*
+    async function start() {
+        try {
+            await mongoose.connect(config.get('mongoURI'), {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+            });
+            app.listen(PORT, () => console.log(`Server was running on port ${PORT}`));
+            
+        } catch (e) {
+            console.log('Server error', e.message);
+            process.exit(1);
+        }
+    }
+*/
+
+// Start server without Mongo
+
+
+function start() {
+	try {
+		app.listen(PORT, () => console.log(`Server was running on port ${PORT}`));
+	} catch (e) {
+		console.log('Server error', e.message);
+		process.exit(1);
+	}
+}
+
+start();
