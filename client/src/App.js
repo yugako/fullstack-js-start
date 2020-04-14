713c1a6159568f77c41d6158baba9c63a2e7fefc
Yurii Koltso <yura.k.upqode@gmail.com>
2020-04-14 22:19:57 +0300

    Base markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <div className="App-links">
+          <a
+            className="App-link mongo"
+            href="https://www.mongodb.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Mongo
+          </a>
+          <a
+            className="App-link express"
+            href="https://expressjs.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Express
+          </a>
+          <a
+            className="App-link react"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <a
+            className="App-link node"
+            href="https://nodejs.org/en/docs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Node
+          </a>
+        </div>
       </header>
     </div>
   );
